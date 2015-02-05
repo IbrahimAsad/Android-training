912888c5ef8c7ae4c55b9a9a5eaf3507e9f4c91d
Ibrahim Asad <ibra.asad@gmail.com>
2015-02-05 02:03:24 +0200

    clear Login activity

--- app/src/main/java/ibrahim/driverstasks/core/APIConstant.java
+++ app/src/main/java/ibrahim/driverstasks/core/APIConstant.java
@@ -1,10 +1,13 @@
 package ibrahim.driverstasks.core;
 
+import org.json.JSONObject;
+
 /**
  * Created by Ibrahim on 2/5/2015.
  */
 public class APIConstant{
 
 public static String path="";
+public static JSONObject response;
 
 }
--- app/src/main/java/ibrahim/driverstasks/core/ServerCall.java
+++ app/src/main/java/ibrahim/driverstasks/core/ServerCall.java
@@ -4,4 +4,9 @@ package ibrahim.driverstasks.core;
  * Created by Ibrahim on 2/5/2015.
  */
 public class ServerCall {
+
+
+    public static void makeCall(){
+
+    }
 }
