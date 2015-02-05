867bf541eb7f423ef5551488d3c37ccc505fd5fa
Ibrahim Asad <ibra.asad@gmail.com>
2015-02-05 02:07:17 +0200

    .

--- app/src/main/java/ibrahim/driverstasks/LoginActivity.java
+++ app/src/main/java/ibrahim/driverstasks/LoginActivity.java
@@ -30,22 +30,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends Activity  {
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    // UI references.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
