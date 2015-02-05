abb6efcc47a326b84c8d4a6779ed0ff8bb273976
Ibrahim Asad <ibra.asad@gmail.com>
2015-02-05 02:00:33 +0200

    Adding Server Classes

---
+++ app/src/main/java/ibrahim/driverstasks/core/APIConstant.java
@@ -0,0 +1,10 @@
+package ibrahim.driverstasks.core;
+
+/**
+ * Created by Ibrahim on 2/5/2015.
+ */
+public class APIConstant{
+
+public static String path="";
+
+}
---
+++ app/src/main/java/ibrahim/driverstasks/core/ServerCall.java
@@ -0,0 +1,7 @@
+package ibrahim.driverstasks.core;
+
+/**
+ * Created by Ibrahim on 2/5/2015.
+ */
+public class ServerCall {
+}
